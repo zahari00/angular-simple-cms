d6b546b2775c31813abff403fe8b4114bd9b7a6e
Zahary Stoyanov <zahary.stoyanov@sofyma.com>
2020-01-18 11:34:14 +0200

    Laravel and Angular installation

---
+++ backend/database/migrations/2020_01_18_085858_page_block.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class PageBlock extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('page_blocks', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('page_id');
+            $table->unsignedBigInteger('block_id');
+            $table->timestamps();
+
+            $table->foreign('block_id')->references('id')->on('blocks')->onDelete('cascade');
+            $table->foreign('page_id')->references('id')->on('pages')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('page_blocks');
+    }
+}
